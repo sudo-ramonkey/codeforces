cee21211fa9e45512e80ee033a01bb721c4eff9d
astroprieto <71893589+astroprieto@users.noreply.github.com>
2023-10-19 12:09:32 -0600

    Better template :)

--- template.cpp
+++ template.cpp
@@ -1,10 +1,29 @@
+/*
+*:astroprieto
+*:CodeForces Round:
+*:Div:
+*:Problem:
+*:Tags:
+*:Difficulty:
+*/
 #include<iostream>
+//#include<array>
+//#include<vector>
+//#include<algorithm>
+//#include<sets>
+//#include<maps>
 using namespace std;
 
 #define ar array
 #define ll long long
 #define ld long double
-#define all(v) (v.begin(), v.end());
+#define pb push_back
+#define mkp make_pair
+#define l(s) s.size()
+#define all(v) v.begin(), v.end()
+#define debug cout<<"Swag"<<"\n";
+#define fr true
+#define cap false
 
 const int MAX_N = 1e5 + 5;
 
