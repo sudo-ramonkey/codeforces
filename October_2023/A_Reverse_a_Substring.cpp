57610f410923b195621cc6741b29e5e7347eb6e4
astroprieto <71893589+astroprieto@users.noreply.github.com>
2023-10-20 13:14:05 -0600

    To-DO :3

---
+++ October_2023/A_Reverse_a_Substring.cpp
@@ -0,0 +1,45 @@
+/*
+*:astroprieto
+*:CodeForces Round:
+*:Div:
+*:Problem:
+*:Tags:
+*:Difficulty:
+*/
+#include<iostream>
+//#include<array>
+//#include<vector>
+//#include<algorithm>
+//#include<sets>
+//#include<maps>
+using namespace std;
+
+#define ar array
+#define ll long long
+#define ld long double
+#define pb push_back
+#define mkp make_pair
+#define l(s) s.size()
+#define all(v) v.begin(), v.end()
+#define debug cout<<"Swag"<<"\n";
+#define fr true
+#define cap false
+
+const int MAX_N = 1e5 + 5;
+
+void ans()
+{
+    debug
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int t;cin>>t;
+    while(t-- > 0)
+    {
+        ans();
+    }
+    return 0;
+}
